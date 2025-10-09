e30f2416adb8ce06246721ec05a1dbec2ebc1a35
vumenira <bodkapavuchok@gmail.com>
2025-10-09 20:36:39 +0300

    finished ultra last

--- insert_data.sql
+++ insert_data.sql
@@ -1,13 +1,34 @@
 # clients, beauty_creators, inventory, schedule, price_list
-CREATE DATABASE BEAUTYBASE;
+
+  CREATE DATABASE BEAUTYBASE;
 USE BEAUTYBASE;
 
+CREATE TABLE clients (
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(50),
+    gender VARCHAR(50)
+);
+
+
+CREATE TABLE beauty_creators (
+    master_id INT PRIMARY KEY,
+    master_name VARCHAR(50),
+    works_since DATE
+);
 
 CREATE TABLE inventory (
         item_id INT PRIMARY KEY,
         item_name VARCHAR(50),
         quantity INT
 
+);
+
+CREATE TABLE price_list (
+    service_id INT PRIMARY KEY,
+    service_price INT,
+    service_name VARCHAR(50),
+    item_id INT,
+    FOREIGN KEY (item_id) REFERENCES inventory(item_id)
 );
 
 CREATE TABLE schedule (
@@ -17,5 +38,5 @@ CREATE TABLE schedule (
         master_id INT,
         FOREIGN KEY (client_id) REFERENCES clients(client_id),
         FOREIGN KEY (service_id) REFERENCES price_list(service_id),
-        FOREIGN KEY (master_id) REFERENCES price_list(master_id)
+        FOREIGN KEY (master_id) REFERENCES beauty_creators(master_id)
 );
