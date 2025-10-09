a67f5af97f250144364f6ff3fcd63a43ae3d93f2
MikeIsupov <misupov@kse.org.ua>
2025-10-09 20:33:21 +0300

    added inventory and schedule tables

--- insert_data.sql
+++ insert_data.sql
@@ -7,10 +7,15 @@ CREATE TABLE inventory (
         item_id INT PRIMARY KEY,
         item_name VARCHAR(50),
         quantity INT
+
 );
 
 CREATE TABLE schedule (
         date DATETIME PRIMARY KEY,
         client_id INT,
-        FOREIGN KEY (client_id) REFERENCES clients(client_id)
+        service_id INT,
+        master_id INT,
+        FOREIGN KEY (client_id) REFERENCES clients(client_id),
+        FOREIGN KEY (service_id) REFERENCES price_list(service_id),
+        FOREIGN KEY (master_id) REFERENCES price_list(master_id)
 );
