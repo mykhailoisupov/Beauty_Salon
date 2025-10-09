61cebe131693cb2daa95ed8c762173cdb5b8e8dc
MikeIsupov <misupov@kse.org.ua>
2025-10-09 20:26:32 +0300

    added inventory and schedule tables

--- insert_data.sql
+++ insert_data.sql
@@ -1,2 +1,16 @@
 # clients, beauty_creators, inventory, schedule, price_list
+CREATE DATABASE BEAUTYBASE;
+USE BEAUTYBASE;
 
+
+CREATE TABLE inventory (
+        item_id INT PRIMARY KEY,
+        item_name VARCHAR(50),
+        quantity INT
+);
+
+CREATE TABLE schedule (
+        date DATETIME PRIMARY KEY,
+        client_id INT,
+        FOREIGN KEY (client_id) REFERENCES clients(client_id)
+);
